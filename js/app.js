9843a1e15193125b480d22b1419e47d6796b6263
adamsebetich@yahoo.com <adamsebetich@yahoo.com>
2015-11-15 14:40:08 -0800

    Adam added password strength

--- js/app.js
+++ js/app.js
@@ -11,4 +11,32 @@ angular.module('FormApp', [])
   		alert("There was an error submitting your payment. Please check that your information is correct.");
   	}
   }
+  var myApp = angular.module('myApp',[]);
+
+  function MyCtrl($scope) {}
 }])
+
+.controller('ctrl', function($scope){
+  $scope.pass="";
+  $scope.v=function(){
+    return test($scope.pass);
+  };
+});
+
+function test(pass){
+  if(pass == null)
+    return -1;
+  var secure = 0;
+  if(pass.length<6)
+    return 0;
+  if( /[0-9]/.test( pass ) )
+    secure++;
+  if( /[a-z]/.test( pass ) )
+    secure++;
+  if( /[A-Z]/.test( pass ) )
+    secure++;
+  if( /[^A-Z-0-9]/i.test( pass ) )
+    secure++;
+  return secure;
+}
+
