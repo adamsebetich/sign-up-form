9c8f176de54170b3f01613d740bf2d0acd15b638
Kelsey Aiello <kelseymaiello@gmail.com>
2015-11-15 12:03:36 -0800

    Kelsey added the email validation, submit and reset button, and updated the app.js file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-app.js
+'use strict';
+
+angular.module('FormApp', [])
+.controller('FormCtrl', ['$scope', function($scope) { 
+
+  $scope.submitForm = function(form){
+  	if(form.$valid) {
+  		alert("Payment info has been submitted!");
+  	}
+  	else {
+  		alert("There was an error submitting your payment. Please check that your information is correct.");
+  	}
+  }
+  
+}])
