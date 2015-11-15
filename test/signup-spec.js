cdb7790bc7cc1228571da1e670385fb3a0e6967f
DGriffing <diana.griffing@gmail.com>
2015-11-15 13:31:57 -0800

    Adds Email Tests, ready to test out

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -13,36 +13,47 @@ describe('Sign Up Form', function(){
 
   // BIRTHDATE TESTS
 
+
+
   // EMAIL TESTS
   // Correct EMAILS should work
   it('should pass valid emails', function(){
-
-      expect( browser.getTitle() ).toEqual('Watch List');
-
-      browser.getTitle().then(function(response){
-          console.log(response);
-      });
-
-  });
-  // Incorrect EMAILS shouldn't work
-  it('should fail invalid emails', function(){
-
     var first =  element(by.model('firstName'));
     searchBar.sendKeys('Jane');
 
     var last =  element(by.model('lastName'));
     searchBar.sendKeys('Doe');
 
-    var emailInput = element(by.model('email'));
+    // good email
+    var email = element(by.model('email'));
+    searchBar.sendKeys('jane@doe.com');
+
+    var button = element(by.id('sign-me-up-button'));
+    button.click();
+
+    // does it fail?
+    var error = element(by.model('email-error'));
+    expect( error.isDisplayed() ).toEqual(false);
+  });
+
+  // Incorrect EMAILS shouldn't work
+  it('should fail invalid emails', function(){
+    var first =  element(by.model('firstName'));
+    searchBar.sendKeys('Jane');
+
+    var last =  element(by.model('lastName'));
+    searchBar.sendKeys('Doe');
+
+    // bad email
+    var email = element(by.model('email'));
     searchBar.sendKeys('bad.email.doe.com');
 
     var button = element(by.id('sign-me-up-button'));
     button.click();
 
     // does it fail?
-    var error = element(by.css('.modal-body'));
-    expect( modalBody.isPresent() ).toEqual(true);
-
+    var error = element(by.model('email-error'));
+    expect( error.isDisplayed() ).toEqual(true);
   });
 
 
