124dbb938b3baa8a75bd8bb46280ab7997baf2a5
Halle Friedland <halle.friedland@gmail.com>
2015-11-15 18:13:08 -0800

    Halle adds birthdate section

--- js/app.js
+++ js/app.js
@@ -24,8 +24,9 @@ FormApp.controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
   	}
   }
 
+  //Compares user's birthday to today's date and checks if user is 13+ 
   $scope.birthdateInput = function(birthdate){
-  	var todaysDate = new Date();
+  	var todaysDate = new Date(); 
   	var usersBirthdate = new Date(birthdate);
   	var dayToday = todaysDate.getDate();
   	var userDay = usersBirthdate.getDate();
@@ -33,29 +34,25 @@ FormApp.controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
   	var userMonth = usersBirthdate.getMonth();
   	var yearToday = todaysDate.getFullYear();
   	var userYear = usersBirthdate.getFullYear();
-  	if(yearToday - userYear > 13) {
+  	//If user's birthdate year is over 13 years before today's year, return true
+  	if(yearToday - userYear > 13) { 
   		return true;
   	}
+  	//If user's birthdate year is 2002 and the user's birthday month has already 
+  	//passed in this calendar year, return true. 
   	if(userYear == 2002 && monthToday > userMonth) {
   		return true; 
   	}
+  	//If user's birthdate year is 2002 and the month of the user's bday is the same
+  	//as this year's current month and the the user's day of birth already passed or is 
+  	//today in this current year, return true
   	if(userYear == 2002 && userMonth == monthToday && dayToday >= userDay) {
   		return true; 
   	}
+  	//otherwise the user is under 13, return false 
   	return false;
   }
-  
-  $scope.birthdateInput = function(birthdate){
-    var todaysDate = Date.parse('11/15/2015');
-    var userBirthdate = Date.parse(birthdate);
-    var ageOfUser = todaysDate - userBirthdate; 
-    var convertToYears = ageOfUser / 31536000000; 
-    if(convertToYears >= 13) {
-      return true;
-    } else {
-      return false; 
-    }
-  }
+
 }])
 
 
