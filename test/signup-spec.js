fc8882dccf7af6c65747b62c2a7ab530ccc6432b
DGriffing <diana.griffing@gmail.com>
2015-11-15 18:37:19 -0800

    writes many tests, semi-functional

--- test/signup-spec.js
+++ test/signup-spec.js
@@ -15,28 +15,39 @@ describe('Sign Up Form', function(){
 
     var last =  element(by.model('lastName'));
     last.sendKeys('Doe');
+    // aaand Test Last Name!
+    var error = element(by.model('last-name-error'));
+    expect( error.isPresent() ).toEqual(false);
 
     var date = element(by.model('birthdate'));
     last.sendKeys('11/14/2002');
+    // aaand Test Birthdate!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresent() ).toEqual(false);
 
     var email = element(by.model('email'));
     email.sendKeys('jane@doe.com');
+    // aaand Test Email!
+    var error = element(by.model('email-error'));
+    expect( error.isPresent() ).toEqual(false);
+
+    var password = element(by.model('password'));
+    email.sendKeys('paSSword123!');
+    // aaand Test Password!
+    var error = element(by.model('password-error'));
+    expect( error.isPresent() ).toEqual(false);
+
 
     var button = element(by.id('sign-me-up-button'));
     button.click();
 
-    var password = element(by.model('password'));
-    email.sendKeys('paSSword123!');
-
-    // aaand Test!
-    var error = element(by.model('email-error'));
-    expect( error.isPresent() ).toEqual(false);
   });
 
 
-  // NAME TESTS
-  it('should give error for name names', function(){
 
+  // NAME TESTS
+  it('should give error for name', function(){
+    // NO LAST NAME
     // aaand Test!
     var error = element(by.model('last-name-error'));
     expect( error.isPresemt() ).toEqual(true);
@@ -44,15 +55,78 @@ describe('Sign Up Form', function(){
 
 
   // BIRTHDATE TESTS
+  it('should give error bad birthday', function(){
+    var month = todaysDate.getMonth();
+    var day = todaysDate.getDate();
+    var year = todaysDate.getFullYear();
+
+    // NO BIRTHDAY
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(true);
+
+    // Date is nonsense
+    var date = element(by.model('birthdate'));
+    date.sendKeys('IAMNOTADATE');
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(true);
+
+    // Date is too young (1 - by one day)
+    var date = element(by.model('birthdate'));
+    date.sendKeys(month + '/' + (day + 1) + '/' + (year - 13));
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(true);
+
+    // Date is too young (2 - by one month)
+    var date = element(by.model('birthdate'));
+    date.sendKeys((month + 1) + '/' + day + '/' + (year - 13));
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(true);
+
+    // Date is too young (3 - by one year)
+    var date = element(by.model('birthdate'));
+    date.sendKeys(month + '/' + day + '/' + (year - 12));
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(true);
+
+     // Date is GOOD (1 - by one day)
+    var date = element(by.model('birthdate'));
+    date.sendKeys(month + '/' + day + '/' + (year - 13));
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(false);
+
+    // Date is GOOD (2 - by one month)
+    var date = element(by.model('birthdate'));
+    date.sendKeys((month - 1) + '/' + day + '/' + (year - 13));
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(false);
+
+    // Date is GOOD (3 - by one year)
+    var date = element(by.model('birthdate'));
+    date.sendKeys(month + '/' + day + '/' + (year - 14));
+    // aaand Test!
+    var error = element(by.model('birthdate-error'));
+    expect( error.isPresemt() ).toEqual(false);
+  });
 
 
 
   // EMAIL TEST: should produce error
   it('should give error for bad email', function(){
-    // bad email
+    // NO EMAIL
+    // aaand Test!
+    var error = element(by.model('email-error'));
+    expect( error.isPresemt() ).toEqual(true);
+
+    // Email without an @ sign
     var email = element(by.model('email'));
     email.sendKeys('bad.email.doe.com');
-
     // aaand Test!
     var error = element(by.model('email-error'));
     expect( error.isPresemt() ).toEqual(true);
