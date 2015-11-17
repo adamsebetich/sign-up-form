a01b867e030e2482c97e26a492dab6ae56ecb02d
adamsebetich@yahoo.com <adamsebetich@yahoo.com>
2015-11-16 20:59:40 -0800

    Final changes

--- js/app.js
+++ js/app.js
@@ -68,29 +68,3 @@ FormApp.controller('FormCtrl', ['$scope', '$http', function($scope, $http) {
   }
 
 }])
-
-
-.controller('ctrl', function($scope){
-  $scope.pass="";
-  $scope.v=function(){
-    return test($scope.pass);
-  };
-});
-
-function test(pass){
-  if(pass == null)
-    return -1;
-  var secure = 0;
-  if(pass.length<6)
-    return 0;
-  if( /[0-9]/.test( pass ) )
-    secure++;
-  if( /[a-z]/.test( pass ) )
-    secure++;
-  if( /[A-Z]/.test( pass ) )
-    secure++;
-  if( /[^A-Z-0-9]/i.test( pass ) )
-    secure++;
-  return secure;
-}
-
